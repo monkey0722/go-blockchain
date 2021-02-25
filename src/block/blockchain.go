b9f42828ab40e804e8e069e40cd9f9e1e1f51bd3
monkey0722 <ta19028@gmail.com>
2021-02-25 21:13:56 +0900

    feat: Validate the transaction

--- src/block/blockchain.go
+++ src/block/blockchain.go
@@ -1,12 +1,15 @@
 package block
 
 import (
+	"crypto/ecdsa"
 	"crypto/sha256"
 	"encoding/json"
 	"fmt"
 	"log"
 	"strings"
 	"time"
+
+	"../utils"
 )
 
 // Constans.
@@ -112,9 +115,42 @@ func (bc *Blockchain) Print() {
 }
 
 // AddTransaction -> Add Transaction to Blockchain
-func (bc *Blockchain) AddTransaction(sender string, recipient string, value float32) {
+func (bc *Blockchain) AddTransaction(
+	sender string,
+	recipient string,
+	value float32,
+	senderPublicKey *ecdsa.PublicKey,
+	s *utils.Signature) bool {
 	t := NewTransaction(sender, recipient, value)
-	bc.transactionPool = append(bc.transactionPool, t)
+
+	if sender == MiningSender {
+		bc.transactionPool = append(bc.transactionPool, t)
+		return true
+	}
+
+	if bc.VerifyTransactionSignature(senderPublicKey, s, t) {
+		/*
+			if bc.CalculateTotalAmount(sender) < value {
+				log.Println("ERROR: Not enough balance in a wallet")
+				return false
+			}
+		*/
+		bc.transactionPool = append(bc.transactionPool, t)
+		return true
+	} else {
+		log.Println("ERROR: Verify Transaction")
+	}
+	return false
+}
+
+// VerifyTransactionSignature -> Verify the transaction signature.
+func (bc *Blockchain) VerifyTransactionSignature(
+	senderPublicKey *ecdsa.PublicKey,
+	s *utils.Signature,
+	t *Transaction) bool {
+	m, _ := json.Marshal(t)
+	h := sha256.Sum256([]byte(m))
+	return ecdsa.Verify(senderPublicKey, h[:], s.R, s.S)
 }
 
 // CopyTransactionPool -> Copy the TransactionPool.
@@ -150,7 +186,7 @@ func (bc *Blockchain) ProofOfWork() int {
 
 // Mining -> Mining.
 func (bc *Blockchain) Mining() bool {
-	bc.AddTransaction(MiningSender, bc.blockchainAddress, MiningReward)
+	bc.AddTransaction(MiningSender, bc.blockchainAddress, MiningReward, nil, nil)
 	nonce := bc.ProofOfWork()
 	previousHash := bc.LastBlock().Hash()
 	bc.CreateBlock(nonce, previousHash)
