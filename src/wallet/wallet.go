ec28459af0601152441420b1c7f71bb33b803299
monkey0722 <ta19028@gmail.com>
2021-03-08 22:38:53 +0900

    feat: Create a transaction

--- src/wallet/wallet.go
+++ src/wallet/wallet.go
@@ -145,3 +145,24 @@ func (t *Transaction) MarshalJSON() ([]byte, error) {
 		Value:     t.value,
 	})
 }
+
+// TransactionRequest -> Type Definition.
+type TransactionRequest struct {
+	SenderPrivateKey           *string `json:"sender_private_key"`
+	SenderBlockchainAddress    *string `json:"sender_blockchain_address"`
+	RecipientBlockchainAddress *string `json:"recipient_blockchain_address"`
+	SenderPublicKey            *string `json:"sender_public_key"`
+	Value                      *string `json:"value"`
+}
+
+// Validate -> Validation of TransactionRequest
+func (tr *TransactionRequest) Validate() bool {
+	if tr.SenderPrivateKey == nil ||
+		tr.SenderBlockchainAddress == nil ||
+		tr.RecipientBlockchainAddress == nil ||
+		tr.SenderPublicKey == nil ||
+		tr.Value == nil {
+		return false
+	}
+	return true
+}
